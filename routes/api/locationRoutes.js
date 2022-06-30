a335de32f724d28931b098b1b99a5105356744e1
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-29 21:32:25 -0500

    testing heroku deploy as well as updateding location routes

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 const { Location, Traveller, Trip } = require('../../models');
 
+// GET all locations
+router.get('/', async (req, res) => {
+    try {
+        const locationData = await Location.findAll();
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
