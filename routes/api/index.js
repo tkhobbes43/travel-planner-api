4afb1f111a53b19231dea1f56e4e44a430e68092
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-29 19:43:22 -0500

    api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const travellerRoutes = require('./travellerRoutes');
+const locationRoutes = require('./locationRoutes');
+const tripRoutes = require('./tripRoutes');
 
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
+router.use('/travellers', travellerRoutes);
+router.use('/locations', locationRoutes);
+router.use('/trips', tripRoutes);
 
 module.exports = router;
